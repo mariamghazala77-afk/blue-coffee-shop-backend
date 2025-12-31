c0bebfd1bffb9484034c98e1f05454150023936d
mariamghazala77-afk <mariamghazala77@gmail.com>
2025-12-31 22:05:04 +0200

    Disable S3 to prevent server crash

--- middleware/upload.js
+++ middleware/upload.js
@@ -1,43 +1,7 @@
 import multer from "multer";
-import multerS3 from "multer-s3";
-import AWS from "aws-sdk";
-import path from "path";
 
-console.log("DEBUG S3_BUCKET =", process.env.S3_BUCKET);
-
-// Configure S3 (Railway Bucket)
-const s3 = new AWS.S3({
-  endpoint: process.env.S3_ENDPOINT,
-  accessKeyId: process.env.S3_KEY,
-  secretAccessKey: process.env.S3_SECRET,
-  region: process.env.S3_REGION,
-  signatureVersion: "v4",
-});
-
-// Multer S3 storage
 const upload = multer({
-  storage: multerS3({
-    s3: s3,
-    bucket: process.env.S3_BUCKET,
-    acl: "public-read",
-    contentType: multerS3.AUTO_CONTENT_TYPE,
-    key: (req, file, cb) => {
-      const fileName =
-        Date.now().toString() + path.extname(file.originalname);
-      cb(null, fileName);
-    },
-  }),
-  fileFilter: (req, file, cb) => {
-    if (
-      file.mimetype === "image/jpeg" ||
-      file.mimetype === "image/png" ||
-      file.mimetype === "image/jpg"
-    ) {
-      cb(null, true);
-    } else {
-      cb(new Error("Only image files are allowed"), false);
-    }
-  },
+  storage: multer.memoryStorage(),
 });
 
 export default upload;
